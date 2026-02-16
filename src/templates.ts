f9df9dd8dce98eda41c126d6c897e8b860871662
maruan <maruanbakriottoni@gmail.com>
2026-02-16 18:02:05 -0300

    add assets to all posts

--- src/templates.ts
+++ src/templates.ts
@@ -198,8 +198,8 @@ export const base = (
               delimiters: [
                 { left: "$$", right: "$$", display: true },
                 { left: "$", right: "$", display: false },
-                { left: "\\(", right: "\\)", display: false },
-                { left: "\\[", right: "\\]", display: true },
+                { left: "\\\\(", right: "\\\\)", display: false },
+                { left: "\\\\[", right: "\\\\]", display: true },
               ],
               throwOnError: false,
               ignoredTags: [
